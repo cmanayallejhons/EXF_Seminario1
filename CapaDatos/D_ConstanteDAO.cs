defc14fc00a82fb5673fcc29e14ee188a9229cd2
Gaby Gutierrez Balcazar <gaby.balcazar.13@gmail.com>
2020-07-28 03:27:09 -0500

    GABY

---
+++ CapaDatos/D_ConstanteDAO.cs
@@ -0,0 +1,33 @@
+﻿using CapaEntidades.Cache;
+using CapaEntidades.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    class D_ConstanteDAO : Conexion
+    {
+        public void insertarConstante(E_CONSTANTE constante)
+        {
+            using (var conexion = GetConnection())
+            {
+                conexion.Open();
+                using (var comand = new SqlCommand())
+                {
+                    comand.Connection = conexion;
+                    comand.CommandText = "SP_INSERTAR_CONSTANTE";
+                    comand.Parameters.AddWithValue("@CN_NOMBRE", constante.cN_NOMBRE);
+                    comand.Parameters.AddWithValue("@CN_CATEGORIA", constante.cN_CATEGORIA);
+                  
+                    comand.CommandType = CommandType.StoredProcedure;
+                    comand.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ CapaDatos/D_ExamenDAO.cs
@@ -0,0 +1,33 @@
+﻿using CapaEntidades.Cache;
+using CapaEntidades.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CapaDatos
+{
+    class D_ExamenDAO : Conexion
+    {
+        public void insertarExamen(E_NIVEL nivel , E_CONSTANTE constante)
+        {
+            using (var conexion = GetConnection())
+            {
+                conexion.Open();
+                using (var comand = new SqlCommand())
+                {
+                    comand.Connection = conexion;
+                    comand.CommandText = "SP_INSERTAR_EXAMEN";
+                    comand.Parameters.AddWithValue("@NI_IDNIVEL", nivel.nI_IDNIVEL);
+                    comand.Parameters.AddWithValue("@CN_IDTIPOCATEGORIA", constante.cN_IDCONSTANTE );
+                    comand.CommandType = CommandType.StoredProcedure;
+                    comand.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
