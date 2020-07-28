5ccc82d105e5c73a41a5d75f6580f370486b9281
daniel paico chileno <daniel_pch98@hotmail.com>
2020-07-28 02:25:17 -0500

    Cpa Negocio - N_Persona

---
+++ CapaNegocio/N_Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using CapaEntidades.DTO;
+
+namespace CapaNegocio
+{
+    public class N_Persona
+    {
+        D_PersonaDAO personaDAO = new D_PersonaDAO();
+
+        public E_PERSONA perfilPersona()
+        {
+            return personaDAO.perfilPersona();
+        }
+
+        public E_PERSONAL buscarPersonal(int documento)
+        {
+            return personaDAO.buscarPersonal(documento);
+        }
+    }
+}
